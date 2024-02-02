f0d41b7c19efb7dd78a49b88e9f3b47bfc6716b0
Carlos Garcia <carlos.garcia@nubank.com.br>
2024-02-02 17:30:03 -0600

    Cleaned up repo bc of slight copy error

--- nvim/lua/cxrlos/init.lua
+++ nvim/lua/cxrlos/init.lua
@@ -1,2 +1,3 @@
 require("cxrlos.remap")
 require("cxrlos.packer")
+require("cxrlos.set")
--- nvim/lua/cxrlos/packer.lua
+++ nvim/lua/cxrlos/packer.lua
@@ -2,34 +2,65 @@
 vim.cmd.packadd("packer.nvim")
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use "wbthomason/packer.nvim"
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim"
 
-	use {	-- Telescope: Fuzzy finder
-		"nvim-telescope/telescope.nvim", tag = "0.1.5",
-		requires = { {"nvim-lua/plenary.nvim"} }
-	}
-	use ({	-- Rose Pine: Color scheme
-		"rose-pine/neovim",
-		as = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end
-	})
-	use (	-- Treesitter: Syntax highlighting
-		"nvim-treesitter/nvim-treesitter", 
-		{run = ":TSUpdate"}
-	)
-	use (	-- Treesitter Playground: Treesitter playground
-		"nvim-treesitter/playground"
-	)
-	use (	-- Harpoon: Quick file navigation
-		"theprimeagen/harpoon"
-	)
-	use (	-- UndoTree: Visualize undo history
-		"mbbill/undotree"
-	)
-	use {	-- GitHub Copilot: AI pair programming
-		"github/copilot.vim", branch = "release" 
-	}
+    use {	-- Telescope: Fuzzy finder
+    "nvim-telescope/telescope.nvim", tag = "0.1.5",
+    requires = { {"nvim-lua/plenary.nvim"} }
+}
+use ({	-- Rose Pine: Color scheme
+"rose-pine/neovim",
+as = "rose-pine",
+config = function()
+    vim.cmd("colorscheme rose-pine")
+end
+    })
+    use (	-- Treesitter: Syntax highlighting
+    "nvim-treesitter/nvim-treesitter", 
+    {run = ":TSUpdate"}
+    )
+    use (	-- Treesitter Playground: Treesitter playground
+    "nvim-treesitter/playground"
+    )
+    use (	-- Harpoon: Quick file navigation
+    "theprimeagen/harpoon"
+    )
+    use (	-- UndoTree: Visualize undo history
+    "mbbill/undotree"
+    )
+    use (	-- Vim-Fugitive: Git wrapper
+    "tpope/vim-fugitive"
+    )
+    use (  -- Git Signs: Git signs
+    'mfussenegger/nvim-dap'
+    )
+    use {	-- LSP Zero: LSP configuration
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-nvim-lua'},
+
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},
+        {'rafamadriz/friendly-snippets'},
+
+        use({'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" }}),
+
+        }
+    }
+    use {	-- GitHub Copilot: AI pair programming
+        "github/copilot.vim", branch = "release"
+    }
 end)
