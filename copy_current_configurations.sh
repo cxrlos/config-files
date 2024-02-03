5a3da815bbd160d39627cbb185c5c2b129b4fe25
Carlos Garcia <carlos.garcia@nubank.com.br>
2024-02-02 18:02:39 -0600

    Ready to pull

---
+++ copy_current_configurations.sh
@@ -0,0 +1,3 @@
+# This will copy the current configurations for NeoVim and the .zshrc file to the current directory
+cp -r ~/.config/nvim/* ./nvim/.
+cp ~/.zshrc .
---
+++ paste_current_configurations.sh
@@ -0,0 +1,4 @@
+# Paste the current configurations to the home directory
+cp -r ./nvim/* ~/.config/nvim/.
+cp .zshrc ~
+
